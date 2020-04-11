a1587ab0409c66de52310d8abdffed54a715f97c
Craig <craig@RedHpDebian>
2020-04-11 18:52:59 +0100

    Add Fable.Validation and implement basic use

--- src/App.fs
+++ src/App.fs
@@ -5,19 +5,43 @@ open Fable.React
 open Fable.React.Props
 open Fulma
 open Fable.FontAwesome
+open Fable.Validation.Core
 
 type Model =
-    { Value : string }
+    { 
+      Value : string 
+      ValueValidationErrors : string List
+    }
 
 type Msg =
     | ChangeValue of string
+    | UpdateValidationErrors
 
-let init _ = { Value = "" }, Cmd.none
+let init _ = { Value = "" ; ValueValidationErrors = []}, Cmd.none
+
+let validateValue value = 
+      all <| fun t ->
+          t.Test "Value" value // call `t.Test fieldName value` to initialize field state
+                |> t.Trim // pipe the field state to rules
+                |> t.NotBlank "name cannot be blank" // rules can contain params and a generic error message
+                |> t.MaxLen 20 "maxlen is {len}"
+                |> t.MinLen 4 "minlen is {len}"
+                |> t.End // call `t.End` to unwrap the validated
+                         // and transformed value,
+                         // you can use the transformed values to create a new model
+
 
 let private update msg model =
     match msg with
     | ChangeValue newValue ->
-        { model with Value = newValue }, Cmd.none
+        { model with Value = newValue }, Cmd.ofMsg UpdateValidationErrors
+    | UpdateValidationErrors -> 
+        let result = validateValue model.Value
+        match result with 
+        | Ok _ -> 
+          { model with ValueValidationErrors = [] }, Cmd.none
+        | Error (result:Map<string,string list>) ->  
+          { model with ValueValidationErrors = result.["Value"] }, Cmd.none
 
 let private view model dispatch =
     Hero.hero [ Hero.IsFullHeight ]
@@ -36,6 +60,13 @@ let private view model dispatch =
                                 [ Input.text [ Input.OnChange (fun ev -> dispatch (ChangeValue ev.Value))
                                                Input.Value model.Value
                                                Input.Props [ AutoFocus true ] ] ] ]
+                          Content.content [ ]
+                            [ str "Errors: "
+                              model.ValueValidationErrors |> String.concat ", " |> str
+                              str " "
+                              Icon.icon [ ]
+                                [ Fa.i [ Fa.Regular.Smile ]
+                                    [ ] ] ]                        
                           Content.content [ ]
                             [ str "Hello, "
                               str model.Value
